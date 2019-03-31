e46e694ef1d47ccd90b59bb04a0d033b048accca
Stewart ilondanga <stilondanga@gmail.com>
2019-03-31 11:54:23 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,8 +22,8 @@ function s() {
     (e[9].checked ? "<i>" : "") +
     (e[10].checked ? "<mark>" : "") +
     (e[11].checked ? "<pre>" : "") +
-  /*          (e[12].checked ? "<s>" : "") +
-            (e[13].checked ? "<samp>" : "") +
+    (e[12].checked ? "<s>" : "") +
+  /*          (e[13].checked ? "<samp>" : "") +
             (e[14].checked ? "<small>" : "") +
             (e[15].checked ? "<strong>" : "") +
             (e[16].checked ? "<sup>" : "") +

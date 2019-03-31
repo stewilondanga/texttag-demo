597037e6e9b3dc98c3a1be5bd42a3eab86c5d521
Stewart ilondanga <stilondanga@gmail.com>
2019-03-31 12:03:56 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,8 +39,8 @@ function s() {
     (e[10].checked ? "</mark>" : "") +
     (e[9].checked ? "</i>" : "") +
     (e[8].checked ? "</h1>" : "") +
-  /*          (e[7].checked ? "</em>" : "") +
-            (e[6].checked ? "</ins>" : "") +
+    (e[7].checked ? "</em>" : "") +
+  /*          (e[6].checked ? "</ins>" : "") +
             (e[5].checked ? "</del>" : "") +
             (e[4].checked ? "</code>" : "") +
             (e[3].checked ? "</blockquote>" : "") +
